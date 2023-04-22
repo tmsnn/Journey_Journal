180e520044b176c30dcc68ba293b114e7b8227bc
Adonis <98643996+PP221B030145@users.noreply.github.com>
2023-04-22 13:27:47 +0600

    update pipes

--- Frontend/journey_journal_front/src/app/voucher-list/pipes.ts
+++ Frontend/journey_journal_front/src/app/voucher-list/pipes.ts
@@ -3,6 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'filter'
 })
+
 export class FilterPipe implements PipeTransform {
   transform(items: any[], searchText: string): any[] {
     if (!items || !items.length) {
